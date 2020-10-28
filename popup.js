80646489792c223310cc199dd9a0d4ea5b884d94
Greg <60011793+gregtuc@users.noreply.github.com>
2020-10-28 11:22:06 -0400

    Changing structure of proj

--- popup.js
+++ popup.js
@@ -1,73 +1,64 @@
 function requestPage() {
-  //Request and process article content.
   chrome.tabs.query(
-    { active: true, windowId: chrome.windows.WINDOW_ID_CURRENT },
-    function (tabs) {
-      const { id: tabId } = tabs[0].url;
-      let code = `var result = document.querySelectorAll(
-        'div[class*="StoryBodyCompanionColumn"], div[data-testid="photoviewer-children"]'
-      );var resulthtml = [];for (var i = 0; i < result.length; i++) {resulthtml.push(result[i].outerHTML);};resulthtml;`;
-      chrome.tabs.executeScript(tabId, { code }, function (result) {
-        filterPage(result[0]);
-      });
+    { active: !0, windowId: chrome.windows.WINDOW_ID_CURRENT },
+    function (e) {
+      const { id: t } = e[0].url;
+      chrome.tabs.executeScript(
+        t,
+        {
+          code:
+            'var result = document.querySelectorAll(\n        \'div[class*="StoryBodyCompanionColumn"], div[data-testid="photoviewer-children"]\'\n      );var resulthtml = [];for (var i = 0; i < result.length; i++) {resulthtml.push(result[i].outerHTML);};resulthtml;',
+        },
+        function (e) {
+          filterPage(e[0]);
+        }
+      );
     }
   );
 }
-
-function filterPage(result) {
-  // result has the return value from `code`
-  if (result) {
-    //Break article text into pieces.
-    var articlePieces = result;
-    //Create paragraphs from content.
-    var content = "";
-    for (var i = 0; i < articlePieces.length; i++) {
-      //Filter dead expand buttons.
-      if (articlePieces[i].indexOf("<svg") !== -1) {
-        articlePieces[i] = articlePieces[i].replace(/((<svg)(.*?)>)/g, " ");
-      }
-      //Filter empty image space.
-      if (articlePieces[i].indexOf('<div style="visibility: hidden;' !== -1)) {
-        articlePieces[i] = articlePieces[i].replace(
-          /((<div style="visibility: hidden;)(.*?))/g,
-          '<div style="visibility: hidden; height:0px;'
-        );
-      }
-      //Filter duplicate images.
-      if (
-        articlePieces[i].indexOf(
-          '<div data-testid="photoviewer-overlay"' !== -1
-        )
-      ) {
-        articlePieces[i] = articlePieces[i].replace(
-          /((<div data-testid="photoviewer-overlay")(.*?)>)/g,
-          ""
-        );
-      }
-      //Filter videos.
-      if (articlePieces[i].indexOf("<video" !== -1)) {
-        articlePieces[i] = articlePieces[i].replace(
-          /((<video)(.*?)>)/g,
-          "<p><br/>Videos cannot be displayed yet on this extension.<br/></p>"
-        );
-      }
-      var insert = "<p>" + articlePieces[i] + "</p>";
-      var content = content + insert;
+function filterPage(e) {
+  if (e.length > 0) {
+    for (var t = e, i = "", n = 0; n < t.length; n++) {
+      -1 !== t[n].indexOf("<svg") &&
+        (t[n] = t[n].replace(/((<svg)(.*?)>)/g, " ")),
+        t[n].indexOf(!0) &&
+          (t[n] = t[n].replace(
+            /((<div style="visibility: hidden;)(.*?))/g,
+            '<div style="visibility: hidden; height:0px;'
+          )),
+        t[n].indexOf(!0) &&
+          (t[n] = t[n].replace(
+            /((<div data-testid="photoviewer-overlay")(.*?)>)/g,
+            ""
+          )),
+        t[n].indexOf(!0) &&
+          (t[n] = t[n].replace(
+            /((<video)(.*?)>)/g,
+            "<p><br/>Videos cannot be displayed yet on this extension.<br/></p>"
+          )),
+        t[n].indexOf(!0) &&
+          (t[n] = t[n].replace(
+            /((<div)(.*?)(data-testid="inline-message")(.*?)>)/g,
+            ""
+          ));
+      i = i + ("<p>" + t[n] + "</p>");
     }
-
-    //Send results
-    displayPage(result);
+    let s = document.querySelector("html");
+    (s.style.minHeight = "100vh"),
+      (s.style.minWidth = "100vh"),
+      displayPage(
+        '<p style="padding: 5px; color: #111; font-family: \'Open Sans\', sans-serif; font-size: 30px; font-weight: 300; line-height: 32px; margin: 0 0 10px; text-align: center;">Note that this extension does not work well on stories with interactive graphs and "Live" stories.</p>' +
+          i
+      );
   } else {
-    var alternate =
-      "<p>Try again when you're being paywalled on a New York Times article!</p>";
-    displaypage(alternate);
+    let e = document.querySelector("html");
+    (e.style.maxHeight = "35px"), (e.style.minWidth = "450px");
+    displayPage(
+      "<p style=\"padding: 5px; color: #111; font-family: 'Open Sans', sans-serif; font-size: 30px; font-weight: 300; line-height: 32px; margin: 0 0 72px; text-align: center;\">This extension must be used on a New York Times article!</p>"
+    );
   }
 }
-
-//Send article content to popup.html
-function displayPage(results) {
-  articleBody = results;
-  document.querySelector("#root").innerHTML = results;
+function displayPage(e) {
+  (articleBody = e), (document.querySelector("#root").innerHTML = e);
 }
-
 requestPage();
